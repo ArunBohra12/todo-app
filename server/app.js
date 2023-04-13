ec8d09bdd4a80ef122748f1b25ac89325ecabdab
Arun Bohra <arunbohra12@gmail.com>
2023-04-13 08:39:41 +0530

    Add root router for the api

--- server/app.js
+++ server/app.js
@@ -1,16 +1,14 @@
 import express from 'express';
 
 import globalErrorHandlingMiddleware from './middlewares/globalErrorHandler.js';
-import authRouter from './routes/authRouter.js';
-import userRouter from './routes/userRouter.js';
+import rootRouter from './routes/rootRouter.js';
 
 const app = express();
 
 // The body parser middleware
 app.use(express.json());
 
-app.use('/api', authRouter);
-app.use('/api/user', userRouter);
+app.use('/api', rootRouter);
 
 app.use(globalErrorHandlingMiddleware);
 
